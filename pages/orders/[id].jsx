56a9e4cd7377cb6e76ad4ee460a4c5c42b3dd068
Reyk Zepper <reyk.zepper@gmail.com>
2023-04-29 10:07:51 +0200

    add shopping card and orders dynamic route

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -1,19 +1,23 @@
-import { Table, CloseButton, Button, Card } from "react-bootstrap";
+import { Table, CloseButton, Button, Card, Spinner } from "react-bootstrap";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
-export default function ShoppingCart() {
+export default function Orders() {
+  const router = useRouter();
+  const { id } = router.query;
+
   return (
     <div>
-      <h1>shopping cart</h1>
+      <h1>Order Status</h1>
       <div className="row mt-4">
         <div className="col-9">
           <Table hover responsive>
             <thead>
               <tr>
-                <th>Image</th>
-                <th>Product</th>
-                <th>Quantity</th>
-                <th>Price</th>
+                <th>Order</th>
+                <th>Customer</th>
+                <th>Address</th>
+                <th>Status</th>
                 <th>
                   <CloseButton disabled />
                 </th>
@@ -21,20 +25,12 @@ export default function ShoppingCart() {
             </thead>
             <tbody>
               <tr>
+                <td>{id}</td>
+                <td>Hape</td>
+                <td>Berlin</td>
                 <td>
-                  <Image
-                    src="/images/products/3d-puzzle.jpg"
-                    alt="3d puzzle"
-                    width={50}
-                    height={50}
-                  />
-                </td>
-                <td>3d Puzzle</td>
-                <td>1</td>
-                <td>10.00</td>
-                <td>
-                  {" "}
-                  <Button className="btn-small">x</Button>{" "}
+                  <span>in Progress</span>
+                  <Spinner animation="border" variant="success" size="sm" />
                 </td>
               </tr>
             </tbody>
@@ -46,7 +42,7 @@ export default function ShoppingCart() {
               <Card.Header as="h5">Order Summary</Card.Header>
               <Card.Body className="text-center">
                 <Card.Title>10,00 $</Card.Title>
-                <Button variant="primary">Checkout</Button>
+                <Button variant="success disabled">payed</Button>
               </Card.Body>
             </Card>
           </div>
