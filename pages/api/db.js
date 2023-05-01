5c3473017d0ee1fa78d778f4810b2c90d0324cdc
Reyk Zepper <reyk.zepper@gmail.com>
2023-05-01 20:27:48 +0200

    implement ssr

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,9 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import mongodb from "@/utils/mongodb";
+import jsondb from "@/jsondb/products";
+import Product from "@/models/Product";
 
-export default function handler(req, res) {
-  mongodb.dbConnect();
-  mongodb.dbDisconnect();
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req, res) {
+  await mongodb.dbConnect();
+  await Product.deleteMany();
+  await Product.insertMany(jsondb.products);
+  const products = await Product.find({});
+  await mongodb.dbDisconnect();
+  res.send(products);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import Slider from "@/components/Slider";
 import ProductList from "@/components/ProductList";
+import mongodb from "@/utils/mongodb";
+import Product from "@/models/Product";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <>
       <Slider />
-      <ProductList />
+      <ProductList products={products} />
     </>
   );
+}
+
+export async function getServerSideProps() {
+  await mongodb.dbConnect();
+  const products = await Product.find({}).lean();
+  return {
+    props: {
+      products,
+    },
+  };
 }
