9b12e336d8a809dc3ee103808eeb50976a953a0f
Reyk Zepper <reyk.zepper@gmail.com>
2023-05-01 22:32:14 +0200

    update _app.js with provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "../components/Layout";
 import "../styles/custom.scss";
+import store from "@/redux/store";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
